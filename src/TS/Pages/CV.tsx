577c1208bddecb1099958cf08a91243db09c1c43
loic-rouzaud <loic.rouzaud@epitech.eu>
2024-01-04 14:10:34 +0100

    [FIX] - Found why the CV didn't appear. (require instead of file-path). Sorry for the responsive and the 1500px. lol

--- src/TS/Pages/CV.tsx
+++ src/TS/Pages/CV.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const CVViewer = () => {
-  const pdfUrl = '../../Documents/CV.pdf';
 
   return (
     <div>
-      <iframe title="CV" src={pdfUrl} width="100%" height="800px" style={{ border: 'none' }} />
+      <iframe title="CV" src={require("../../Documents/CV.pdf")} width="100%" height="1500px" style={{ border: 'none' }} />
     </div>
   );
 };
