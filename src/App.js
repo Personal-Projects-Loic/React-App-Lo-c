2e9660635a085bdab3eb763450b15ef4dc12f8cc
loic-rouzaud <loic.rouzaud@epitech.eu>
2024-01-07 13:11:15 -0500

    [FIX] - BUGS EVERYWHERE 😡

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './TS/Pages/Home.tsx';
 import CV from './TS/Pages/CV.tsx'
-import BurgerMenu from './TS/Components/BurgerMenu.tsx';
 
 function App() {
   return (
     <Router>
-      <div>
-        <BurgerMenu />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<div>Projects Page</div>} />
@@ -18,7 +14,6 @@ function App() {
           <Route path="/contacts" element={<div>Contacts Page</div>} />
           <Route path="/blog" element={<div>Blog Page</div>} />
         </Routes>
-      </div>
     </Router>
   );
 }
--- src/TS/Pages/Home.tsx
+++ src/TS/Pages/Home.tsx
@@ -2,13 +2,14 @@
 import React from "react";
 import Header from "../Components/Header";
 import PersistentDrawerLeft from "../Components/BurgerMenu";
+import BurgerMenu from "../Components/BurgerMenu";
 
 
 const Home = () => {
   return (
     <div>
-      {/* <Header subtitle="Développeur" imageUrl={require("./../../assets/profile-picture.jpg")} /> */}
-      <PersistentDrawerLeft />
+      <Header subtitle="Développeur" imageUrl={require("./../../assets/profile-picture.jpg")} />
+      <BurgerMenu isOpen/>
     </div>
   );
 };
