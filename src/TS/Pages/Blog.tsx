b9b6d42cd81907be4bc593aa264b43eebee295b5
loic-rouzaud <loic.rouzaud@epitech.eu>
2024-01-10 01:06:30 -0500

    [ADD] - Content on the blog

--- src/TS/Pages/Blog.tsx
+++ src/TS/Pages/Blog.tsx
@@ -3,6 +3,7 @@ import Contact from "../Components/ContactBottom";
 import Articles from "../Components/Articles";
 import Soho from "../../assets/Soho.png";
 import newyork from "../../assets/newyork.png";
+import Chinatown from "../../assets/chinatown.png";
 import Title from "../Components/Title";
 import BackArrow from "../Components/BackArrow";
 
@@ -30,6 +31,12 @@ const Blog = () => {
         En 1974, lors de la première visite de Rudy à New York, il est allé directement de l'aéroport JFK avec une valise au sanctuaire mondialement connu des instruments de musique... la 48e rue. Il rêvait depuis longtemps de visiter cet endroit où les musiciens se rassemblaient pour acheter, vendre, parcourir et rêver. Vous pourriez même y croiser un ou plusieurs de vos idoles.
         
         Rudy et sa femme, Fran, ont ouvert 'Rudy's Music Stop' sur la 48e rue à New York en 1978 et ont commencé à transformer leurs passions en une artisanat tangible. Il n'a pas fallu longtemps pour que 'Rudy's Music' devienne un magasin de guitares bien connu et très respecté par les guitaristes du monde entier. L'amour et l'enthousiasme de Rudy pour ce qu'il fait sont contagieux, et sa longue liste de clients réguliers continue de s'allonger."
+      />
+      <Articles
+        imageSrc={Chinatown}
+        title="China Town"
+        text="Avant les années 1800, l'immigration en provenance d'Asie vers les États-Unis était minimale. Cependant, au cours du XIXe siècle, les États-Unis ont connu des migrations massives d'immigrants en provenance de plusieurs pays asiatiques, en particulier de la Chine. Plusieurs facteurs ont déclenché cette immigration à grande échelle. En 1848, de l'or a été découvert en Californie et tout au long des années 1850, les Chinois ont été recrutés comme une source majeure de main-d'œuvre pour les mines. De nombreux Chinois ont également immigré au cours de cette période pour échapper à la Rébellion Taiping, une guerre civile à grande échelle qui a englobé la majeure partie du sud de la Chine. Dans les années 1860, les Chinois ont été recrutés en grand nombre à la fois de Chine et de l'industrie minière de l'ouest américain pour aider à construire la portion du chemin de fer Central Pacific Railroad de la voie ferrée transcontinentale.
+        La communauté chinoise en pleine croissance a continué de s'étendre bien au-delà de ses limites historiques, et d'ici 1980, la communauté chinoise de New York (y compris les quartiers de Brooklyn et de Flushing, Queens) était la plus grande du pays, dépassant celle de San Francisco. La population italienne de Little Italy a connu une contraction spectaculaire à partir des années 1950, lorsque, comme de nombreux Américains, de nombreux membres des classes moyennes et supérieures ont déménagé vers les banlieues en croissance. Little Italy a diminué en taille, ayant été dépassé par Chinatown du sud, et aujourd'hui son noyau est centré autour de Mulberry Street, avec ses nombreux cafés, restaurants, boulangeries et festivals annuels."
       />
       <Contact />
     </>
